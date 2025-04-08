1805391e92a872ca8def751863418cb3dd0d9270
Eric Tsai <eric492718@gmail.com>
2025-04-08 16:34:48 +0800

    test website production performance

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,9 +1,9 @@
 import { z } from "zod";
-// import DriveContents from "./drive-content";
+import DriveContents from "./drive-content";
 import { QUERIES } from "@/server/db/queries";
 import { auth } from "@clerk/nextjs/server";
 import { unauthorized } from "next/navigation";
-// import { AuthButtons } from "./auth-buttons";
+import { AuthButtons } from "./auth-buttons";
 import { BreadcrumbNav } from "./breadcrumb-nav";
 import { ModeToggleButton } from "@/components/client-mode-toggle";
 
@@ -11,42 +11,42 @@ export default async function DrivePage(props: {
   params: Promise<{ folderId: string }>;
 }) {
   const params = await props.params;
-  // const { data, success } = z
-  //   .object({
-  //     folderId: z.coerce.number(),
-  //   })
-  //   .safeParse(params);
+  const { data, success } = z
+    .object({
+      folderId: z.coerce.number(),
+    })
+    .safeParse(params);
 
-  // if (!success) return <div>Invalid folder ID</div>;
+  if (!success) return <div>Invalid folder ID</div>;
 
-  // const parsedFolderId = data.folderId;
+  const parsedFolderId = data.folderId;
 
-  // // 取得該資料夾的 ownerId
-  // const currentFolderOwnerId = await QUERIES.getFolderOwner(parsedFolderId);
-  // const session = await auth();
+  // 取得該資料夾的 ownerId
+  const currentFolderOwnerId = await QUERIES.getFolderOwner(parsedFolderId);
+  const session = await auth();
 
-  // if (currentFolderOwnerId !== session.userId) {
-  //   unauthorized();
-  // }
+  if (currentFolderOwnerId !== session.userId) {
+    unauthorized();
+  }
 
   return (
     <div className="flex h-screen flex-col">
       <div className="flex-1 overflow-hidden p-8">
         <div className="mx-auto flex h-full max-w-6xl flex-col">
           <header className="flex items-center justify-between">
-            {/* <BreadcrumbNav
+            <BreadcrumbNav
               folderId={parsedFolderId}
               currentFolderOwnerId={currentFolderOwnerId}
-            /> */}
+            />
             <div className="flex items-center gap-6">
-              {/* <AuthButtons /> */}
+              <AuthButtons />
               <ModeToggleButton />
             </div>
           </header>
-          {/* <DriveContents
+          <DriveContents
             currentFolderId={parsedFolderId}
             currentFolderOwnerId={currentFolderOwnerId}
-          /> */}
+          />
         </div>
       </div>
     </div>
