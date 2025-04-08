967b71bb83c1c43c7d324919a16a0b01421b55ce
Eric Tsai <eric492718@gmail.com>
2025-04-08 16:07:31 +0800

    test website production performance

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -11,33 +11,33 @@ export default async function DrivePage(props: {
   params: Promise<{ folderId: string }>;
 }) {
   const params = await props.params;
-  const { data, success } = z
-    .object({
-      folderId: z.coerce.number(),
-    })
-    .safeParse(params);
+  // const { data, success } = z
+  //   .object({
+  //     folderId: z.coerce.number(),
+  //   })
+  //   .safeParse(params);
 
-  if (!success) return <div>Invalid folder ID</div>;
+  // if (!success) return <div>Invalid folder ID</div>;
 
-  const parsedFolderId = data.folderId;
+  // const parsedFolderId = data.folderId;
 
-  // 取得該資料夾的 ownerId
-  const currentFolderOwnerId = await QUERIES.getFolderOwner(parsedFolderId);
-  const session = await auth();
+  // // 取得該資料夾的 ownerId
+  // const currentFolderOwnerId = await QUERIES.getFolderOwner(parsedFolderId);
+  // const session = await auth();
 
-  if (currentFolderOwnerId !== session.userId) {
-    unauthorized();
-  }
+  // if (currentFolderOwnerId !== session.userId) {
+  //   unauthorized();
+  // }
 
   return (
     <div className="flex h-screen flex-col">
       <div className="flex-1 overflow-hidden p-8">
         <div className="mx-auto flex h-full max-w-6xl flex-col">
           <header className="flex items-center justify-between">
-            <BreadcrumbNav
+            {/* <BreadcrumbNav
               folderId={parsedFolderId}
               currentFolderOwnerId={currentFolderOwnerId}
-            />
+            /> */}
             <div className="flex items-center gap-6">
               {/* <AuthButtons /> */}
               <ModeToggleButton />
