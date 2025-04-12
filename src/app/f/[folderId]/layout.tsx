3a74431b4d52335fb4342b4e5ee6a360bdade59a
Eric Tsai <eric492718@gmail.com>
2025-04-12 08:04:21 +0800

    add folder page layout

---
+++ src/app/f/[folderId]/layout.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "@/components/footer";
+
+export default function FolderLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <div className="flex-1 overflow-hidden p-8">
+        <div className="mx-auto flex h-full max-w-6xl flex-col">{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
